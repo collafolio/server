257cecc0c5cc2c4a0131303b8808bc434e7105a2
raram2 <gkfka1189@gmail.com>
2020-08-15 22:35:11 +0900

    Create local server
     - set .env variables
     - create local server and add base middlewares
     - create local db and connect db to server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// create server
+require('dotenv').config();
+require('./db');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json()); // v4.16 이후 내장 모듈로 다시 들어옴 (body-parser 모듈 대체)
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    method: ['GET', 'POST'],
+    credentials: true, // allow cookie
+  }),
+);
+
+app.listen(port, () => {
+  console.log(`Server listen on ${port}`);
+});

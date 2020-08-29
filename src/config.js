c5e6228c518ec6d4f7be8cc4b9b51f6d6163843d
raram2 <gkfka1189@gmail.com>
2020-08-30 04:38:33 +0900

    Add dotenv error handling

--- src/config.js
+++ src/config.js
@@ -1,12 +1,17 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+const dotenvResult = dotenv.config();
+
+if (dotenvResult.error) {
+  throw new Error("Can't find .env file");
+}
 
 module.exports = {
   development: {
-    port: process.env.PORT_LOCAL || 4000,
+    port: process.env.PORT || 4000,
     mongodb: process.env.MONGODB_URI_LOCAL,
   },
   production: {
     port: process.env.PORT || 8080,
-    mongodb: process.env.MONGODB_URI,
+    mongodb: process.env.MONGODB_URI_CLOUD,
   },
 };

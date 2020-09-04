5ec91b50acdb55c0ac3e52a4118927f0c187e5df
raram2 <gkfka1189@gmail.com>
2020-09-04 19:11:18 +0900

    Implement signup
     - make signup verifying middleware
     - apply middleware into signup route
     - make signup controller using email

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
+const { verifySignup } = require('../middlewares');
 const { user } = require('../controllers');
 
 module.exports = (router) => {
-  router.post('/user', user.signup);
+  router.post('/user', [verifySignup.checkDuplicateEmail], user.signup);
   router.delete('/user', user.signout);
 };

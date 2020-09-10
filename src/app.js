da58e1a47aedeea723e49235b507f274cfb2ae56
raram2 <gkfka1189@gmail.com>
2020-09-10 17:34:47 +0900

    Refactor config
     - add client origin to .env
     - refactor config file
     - fix config variables in app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { development, production } = require('./configs/db.config');
+const { local, production } = require('./configs');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const routes = require('./routes');
+
 const productionEnv = process.env.NODE_ENV === 'production';
+const mongodb = productionEnv ? production.mongodb : local.mongodb;
 const morganFormat = productionEnv ? 'combined' : 'dev';
-const port = process.env.PORT || 8080;
-const mongodb = productionEnv ? production.mongodb : development.mongodb;
+const port = local.port || 8080;
 
 console.log(`mode: ${productionEnv ? 'production' : 'development'}`);
 
@@ -15,20 +17,23 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: [local.clientOrigin, production.clientOrigin],
     method: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true, // allow cookies
   }),
 );
 app.use(morgan(morganFormat));
-app.use('/api', routes); // routes
 
-app.listen(port, () => {
-  console.log(`server listen on ${port}`);
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'Welcome to Collafolio server' });
 });
 
+app.use('/api', routes); // set API routes
+
+// Connecting the database and then starting the app.
 mongoose.Promise = global.Promise;
 mongoose
   .connect(mongodb, {
@@ -37,8 +42,13 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('connected to mongodb');
-    // init db here
+    console.log('Successfully connected to mongodb');
+    /*
+      initialize db here
+    */
+    app.listen(port, () => {
+      console.log(`Server listen on ${port}`);
+    });
   })
   .catch((err) => {
     console.error(err);
--- src/configs/index.js
+++ src/configs/index.js
@@ -6,10 +6,16 @@ if (dotenvResult.error) {
 }
 
 module.exports = {
-  development: {
+  auth: {
+    jwtSecret: process.env.JWT_SECRET,
+  },
+  local: {
+    port: process.env.PORT,
+    clientOrigin: process.env.CLIENT_ORIGIN_LOCAL,
     mongodb: process.env.MONGODB_LOCAL,
   },
   production: {
+    clientOrigin: process.env.CLIENT_ORIGIN_CLOUD,
     mongodb: process.env.MONGODB_CLOUD,
   },
 };
